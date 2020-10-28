44d170d09761006e50395ff06fe32f668ae0ae0f
debashis1992 <d2011das@gmail.com>
2020-10-28 12:36:50 +0530

    another commit

--- src/main/kotlin/FortuneCookie.kt
+++ src/main/kotlin/FortuneCookie.kt
@@ -1,15 +1,31 @@
 fun main(args:Array<String>) {
-    println("Your fortune is: ${getFortuneCookie()}")
+    println("Your fortune is: ${ getFortuneCookie(getBirthday()) }")
+//    getFinalFortuneCookie()
 }
 
-fun getFortuneCookie() : String{
+fun getFortuneCookie(birthday: Int) : String {
     val fortunes: List<String> = listOf<String>("You will have a great day!",
     "Things will go well for you today.","Enjoy a wonderful day of success","Be humble and all will turn out well",
     "Today is a good day for exercising restraint.","Take it easy and enjoy life!",
     "Treasure your friends because they are your greatest fortune.")
 
-    print("Enter your birthday:")
-    val birthday = readLine()?.toIntOrNull() ?: 1
-    return fortunes[birthday.div(fortunes.size)];
+    return when(birthday) {
+        in 1..7 -> "You will have a great day"
+        28, 31 -> "Things will go well for you today"
+        else -> fortunes[birthday.rem(fortunes.size)]
+    }
 
+}
+//fun getFinalFortuneCookie() {
+//    var res: String
+//    for(i in 1..10) {
+//        res = getFortuneCookie()
+//        println("Got Fortune : ${ res }")
+//        if(res.contains("Take it easy")) break
+//    }
+//}
+
+fun getBirthday() : Int {
+    print("Enter your birthday:")
+    return readLine()?.toIntOrNull() ?: 1
 }
